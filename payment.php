07ebfdada977b6d354e1e0691ddafa5a082e3263
Piyush Yadav <41717967+Piyush1105@users.noreply.github.com>
2021-11-24 12:00:49 +0530

    clean code

--- payment.php
+++ payment.php
@@ -23,43 +23,7 @@
 		<button type='submit' class='btn btn-primary' name="otp" value="submit" onclick='result()'>Send OTP</button>
 		</form>
 
-		  <!-- <div class='verify-otp'>
-              <label class='' >Enter OTP: </label>
-			  <input type='number' name='otp' class='get-otp' placeholder='Enter OTP' required>
-		    <button type='verify' onclick='submit_otp()'>Verify OTP</button>
-      </div>
-  </div> -->
-<!--   
- <script>
-   function send_otp(){
-     var email = jQuery('#email').val();
-     jQuery.ajax({
-       url: 'card_details.php',
-       type: 'POST',
-       data: 'email='+email,
-       success: function(result){
-         if(result == 'success'){
-           jQuery('.verify-otp').show();
-           jQuery('.container').hide();
-         }
-       }
-     });
-   }
-   function submit_otp(){
-     var otp = jQuery('#email').val();
-     jQuery.ajax({
-       url: 'check_otp.php',
-       type: 'POST',
-       data: 'email='+email,
-       success: function(result){
-         if(result == 'success'){
-           jQuery('.verify-otp').show();
-           jQuery('.container').hide();
-         }
-       }
-     });
-   }
-   </script> -->
+<!-- otp generate to the console using RSA -->
 <script>
   function gcd (a, b)
 {
@@ -112,92 +76,6 @@ function is_prime(num){
     }
     return true
 }
-
-
-// function rel_prime(phi)
-// {
-//    var rel=5;
-//    while (gcd(phi,rel)!=1)
-//       rel++;
-//    return rel;
-// }
-
-
-
-// function power(a, b)
-// {
-//    var temp=1, i;
-//    for(i=1;i<=b;i++)
-//       temp*=a;
-//     return temp;
-// }
-
-
-
-// function encrypt(N, e, M)
-// {
-//    var r,i=0,prod=1,rem_mod=0;
-//    while (e>0)
-//    {
-//       r=e % 2;
-//       if (i++==0)
-//          rem_mod=M % N;
-//       else
-//          rem_mod=power(rem_mod,2) % N;
-//       if (r==1)
-//       {
-//          prod*=rem_mod;
-//          prod=prod % N;
-//       }
-//       e=parseInt(e/2);
-//    }
-//    return prod;
-// }
-
-
-
-// function decrypt(c, d, N)
-// {
-//    var r,i=0,prod=1,rem_mod=0;
-//    while (d>0)
-//    {
-//       r=d % 2;
-//       if (i++==0)
-//          rem_mod=c % N;
-//       else
-//          rem_mod=power(rem_mod,2) % N;
-//       if (r==1)
-//       {
-//          prod*=rem_mod;
-//          prod=prod % N;
-//       }
-//       d=parseInt(d/2);
-//    }
-//    return prod;
-// }
-
-
-
-// function openNew()
-// {
-//    var subWindow=window.open("Output.htm", "Obj","HEIGHT=400,WIDTH=600,SCROLLBARS=YES");
-//    var p=parseInt(document.Input.p.value);
-//    var q=parseInt(document.Input.q.value);
-//    var M=parseInt(document.Input.M.value);
-//    var N=p * q;
-//    var phi=(p-1)*(q-1);
-//    var e=rel_prime(phi);
-//    var c=encrypt(N,e,M);
-//    var d=calculate_d(phi,e);
-//    subWindow.document.Output.N.value=N;
-//    subWindow.document.Output.phi.value=phi;
-//    subWindow.document.Output.e.value=e;
-//    subWindow.document.Output.c.value=c;
-//    subWindow.document.Output.d.value=d;
-//    subWindow.document.Output.M.value=decrypt(c,d,N);
-// }
-
-
 
 function generate_key_pair(p,q){
     p= Math.floor(Math.random() *(1000-1) + 1)
